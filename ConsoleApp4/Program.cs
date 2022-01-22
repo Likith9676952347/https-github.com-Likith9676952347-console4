23ddbe0318064f0f3c02b079a16d412beba258c9
Likith9676952347 <likithkumarreddy143@gmail.com>
2022-01-22 12:14:17 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello" + name);
+
+            Console.WriteLine("how many hours did you sleep?");
+            int hoursofsleep = Convert.ToInt32(Console.ReadLine());
+
+            if (hoursofsleep < 6)
+            {
+                Console.WriteLine("u must be feeling tierd");
+            }
+            else 
+            {
+                Console.WriteLine("u must be feeling enthusastic");
+            }
+        }
+    }
+}
